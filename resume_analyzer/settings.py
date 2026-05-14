ee8161a3dd31d87a7a5bb3f1232904638f9cbd95
Aman Raj <amanshah.here@gmail.com>
2026-05-14 15:38:29 +0530

    Fixed static files for deployment

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
@@ -133,4 +138,3 @@ MESSAGE_TAGS = {
 
 LOGIN_URL = 'login'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
